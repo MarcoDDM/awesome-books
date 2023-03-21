14bfac37e0b3ddd2967e7ca0fadcf544c0f5dc1b
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-03-21 09:03:50 -0300

    Changes to fix linters on js

--- script.js
+++ script.js
@@ -1,62 +1,61 @@
 let books = JSON.parse(localStorage.getItem('books')) || [];
 
 function displayBooks() {
-  let bookList = document.getElementById('book-list');
-  bookList.innerHTML = '';
-  books.forEach(book => {
-    let div = document.createElement('div');
 
-    let title = document.createElement('span');
+  function removeBook(title) {
+    books = books.filter((book) => book.title !== title);
+    localStorage.setItem('books', JSON.stringify(books));
+    displayBooks();
+  }
+  
+  const bookList = document.getElementById('book-list');
+  bookList.innerHTML = '';
+  books.forEach((book) => {
+    const div = document.createElement('div');
+
+    const title = document.createElement('span');
     title.textContent = book.title;
     div.appendChild(title);
     div.appendChild(document.createElement('br'));
 
-    let author = document.createElement('span');
+    const author = document.createElement('span');
     author.textContent = book.author;
     div.appendChild(author);
     div.appendChild(document.createElement('br'));
     div.appendChild(document.createElement('hr'));
 
-
-    let removeBtn = document.createElement('button');
+    const removeBtn = document.createElement('button');
     removeBtn.textContent = 'Remove';
     div.appendChild(removeBtn);
     div.appendChild(document.createElement('br'));
     div.appendChild(document.createElement('br'));
 
-
-    removeBtn.addEventListener('click', () => removeBook(book.title));
+    removeBtn.addEventListener('click', () => { removeBook(book.title); });
 
     bookList.appendChild(div);
   });
 }
 
 function addBook(title, author) {
-  let book = {
-    title: title,
-    author: author,
-    remove: function() {
-      books = books.filter(item => item !== this);
+  const book = {
+    title,
+    author,
+    remove() {
+      books = books.filter(function (item) { return item !== this; });
       localStorage.setItem('books', JSON.stringify(books));
       displayBooks();
-    }
+    },
   };
-  
+
   books.push(book);
   localStorage.setItem('books', JSON.stringify(books));
   displayBooks();
 }
 
-function removeBook(title) {
-  books = books.filter(book => book.title !== title);
-  localStorage.setItem('books', JSON.stringify(books));
-  displayBooks();
-}
-
-document.getElementById('book-form').addEventListener('submit', e => {
+document.getElementById('book-form').addEventListener('submit', (e) => {
   e.preventDefault();
-  let titleInput = document.getElementById('title');
-  let authorInput = document.getElementById('author');
+  const titleInput = document.getElementById('title');
+  const authorInput = document.getElementById('author');
   addBook(titleInput.value, authorInput.value);
   titleInput.value = '';
   authorInput.value = '';
